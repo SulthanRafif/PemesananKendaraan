cb05ae4705ac9f2e96193cf79e12e0e209631b6f
SulthanRafif <rsulthan5@gmail.com>
2021-08-27 14:36:53 +0700

    Membuat Sistem Login

--- website_pemesanan_kendaraan/client/src/Login/index.js
+++ website_pemesanan_kendaraan/client/src/Login/index.js
@@ -7,34 +7,94 @@ function Login() {
     const [passwordReg, setPasswordReg] = useState('');
     const [levelUser, setLevelUser] = useState('');
 
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [loginStatus, setLoginStatus] = useState("");
+    const [level, setLevel] = useState("");
+
+    const register = () => {
+        Axios.post('http://localhost:3001/register', {
+            level_user: levelUser,
+            username: usernameReg,
+            password: passwordReg
+        }).then((response) => {
+            console.log(response)
+        });
+    };
+
+    const login = () => {
+        Axios.post('http://localhost:3001/login', {
+            username: username,
+            password: password
+        }).then((response) => {
+
+            if(response.data.message) {
+                setLoginStatus(response.data.message)
+            } else {
+                setLoginStatus(response.data[0].NAMA_USER)
+                setLevel(response.data[0].LEVEL_USER)
+            }
+        });
+    };
+
     return (
         <div className="App">
             <div className="Login">
                 <h1>Register</h1>
-                <label>Username</label>
-                <input type="text" onChange={(e) => {setUsernameReg(e.target.value)}} />
-                <label>Password</label>
-                <input type="text" onChange={(e) => {setPasswordReg(e.target.value)}}/>
-                <label>Level User</label>
-                <select
+                <div>
+                    <label>Username</label>
+                </div>
+                <div>
+                    <input type="text" onChange={(e) => {setUsernameReg(e.target.value)}} />    
+                </div>
+                <br/>
+                <div>
+                    <label>Password</label>
+                </div>
+                <div>
+                    <input type="text" onChange={(e) => {setPasswordReg(e.target.value)}}/>
+                </div>
+                <br/>
+                <div>
+                    <label>Level User</label>
+                </div>
+                <div>
+                    <select
                       onChange={(e) => {setLevelUser(e.target.value)}}
-                      value="0"
                     >
                       <option value="0">Admin</option>
                       <option value="1">Penyetuju 1</option>
                       <option value="2">Penyetuju 2</option>
                     </select>
-                <button>Register</button>
+                </div>
+                <br/>
+                <div><button onClick={register}>Register</button></div>
             </div> 
             <br/>
+            
             <div className="Login">
                 <h1>Login</h1>
-                <label>Username</label>
-                <input type="text"/>
-                <label>Password</label>
-                <input type="text"/>
-                <button>Login</button>
-            </div>
+                <div>
+                    <label>Username</label>
+                </div>
+                <div>
+                    <input type="text" onChange={(e) => {setUsername(e.target.value)}}/>    
+                </div>
+                <br/>
+                <div>
+                    <label>Password</label>
+                </div>
+                <div>
+                    <input type="text" onChange={(e) => {setPassword(e.target.value)}}/>
+                </div>
+                <br/>
+                <div>
+                    <button onClick={login}>Login</button>
+                </div>
+                <h1>{loginStatus}</h1>
+                <h1>{level}</h1>
+            </div> 
         </div>
     )
 }
--- website_pemesanan_kendaraan/server/index.js
+++ website_pemesanan_kendaraan/server/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const mysql = require("mysql")
+const cors = require("cors")
 
 const app = express()
 
 app.use(express.json());
+app.use(cors());
 
-const db = mysql.createPool({
+const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
@@ -13,7 +15,33 @@ const db = mysql.createPool({
   });
 
   app.post('/register', (req, res)=> {
-      db.query("INSERT INTO user (LEVEL_USER, NAMA_USER, PASSWORD) VALUES (?,?,?)", [level_user, username, password], (err, result))
+      const level_user = req.body.level_user
+      const username = req.body.username
+      const password = req.body.password 
+    
+      db.query("INSERT INTO user (LEVEL_USER, NAMA_USER, PASSWORD) VALUES (?,?,?)", 
+      [level_user, username, password], 
+      (err, result) => {
+          console.log(err);
+      })
+  })
+
+  app.post('/login', (req, res)=> {
+    const username = req.body.username
+    const password = req.body.password 
+  
+    db.query("SELECT * FROM user WHERE NAMA_USER = ? AND PASSWORD = ?", 
+    [username, password], 
+    (err, result) => {
+        if (err) {
+            res.send({err: err})
+        } 
+        if (result.length > 0) {
+            res.send(result)
+        } else {
+            res.send({message: "Wrong username/password combination!"})
+        }
+    })
   })
 
 app.listen(3001, () => {
