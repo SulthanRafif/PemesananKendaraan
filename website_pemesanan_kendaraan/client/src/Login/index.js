3189790495f76f540a043e862dafbf46c32a78e1
SulthanRafif <rsulthan5@gmail.com>
2021-08-27 13:43:57 +0700

    Menginstal Axios

--- website_pemesanan_kendaraan/client/src/Login/index.js
+++ website_pemesanan_kendaraan/client/src/Login/index.js
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Axios from "axios"
 
 function Login() {
+
+    const [usernameReg, setUsernameReg] = useState('');
+    const [passwordReg, setPasswordReg] = useState('');
+    const [levelUser, setLevelUser] = useState('');
+
     return (
         <div className="App">
             <div className="Login">
-                <h1>    
-                    <label>Username</label>
-                    <input type="text"/>
-                    <label>Password</label>
-                    <input type="text"/>
-                </h1>
+                <h1>Register</h1>
+                <label>Username</label>
+                <input type="text" onChange={(e) => {setUsernameReg(e.target.value)}} />
+                <label>Password</label>
+                <input type="text" onChange={(e) => {setPasswordReg(e.target.value)}}/>
+                <label>Level User</label>
+                <select
+                      onChange={(e) => {setLevelUser(e.target.value)}}
+                      value="0"
+                    >
+                      <option value="0">Admin</option>
+                      <option value="1">Penyetuju 1</option>
+                      <option value="2">Penyetuju 2</option>
+                    </select>
+                <button>Register</button>
+            </div> 
+            <br/>
+            <div className="Login">
+                <h1>Login</h1>
+                <label>Username</label>
+                <input type="text"/>
+                <label>Password</label>
+                <input type="text"/>
+                <button>Login</button>
             </div>
         </div>
     )
--- website_pemesanan_kendaraan/server/index.js
+++ website_pemesanan_kendaraan/server/index.js
@@ -12,6 +12,10 @@ const db = mysql.createPool({
     database: "pemesanan_kendaraan",
   });
 
+  app.post('/register', (req, res)=> {
+      db.query("INSERT INTO user (LEVEL_USER, NAMA_USER, PASSWORD) VALUES (?,?,?)", [level_user, username, password], (err, result))
+  })
+
 app.listen(3001, () => {
     console.log('running on port 3001')
 });
