3b30d35f6fecc5910a83ebc6aba96137d9e0826a
SulthanRafif <rsulthan5@gmail.com>
2021-08-30 18:33:02 +0700

    Dari login redirect ke halaman dashboard tapi halaman dashboard belum protected route

--- website_pemesanan_kendaraan/client/src/PemesananAdmin/index.js
+++ website_pemesanan_kendaraan/client/src/PemesananAdmin/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import NavbarAdmin from "../NavbarAdmin";
 import Axios from "axios";
 
@@ -27,6 +27,14 @@ export default class PemesananAdmin extends React.Component {
     });
   };
 
+  validate = () => {
+    let infoSubmit = "";
+    infoSubmit = "Data pemesanan berhasil ditambahkan";
+    this.setState({
+      infoSubmit,
+    });
+  };
+
   handleSubmit = (event) => {
     event.preventDefault();
 
@@ -35,16 +43,17 @@ export default class PemesananAdmin extends React.Component {
     console.log("ID Penyetuju 1: ", this.state.namaPenyetuju1);
     console.log("ID Penyetuju 2: ", this.state.namaPenyetuju2);
     console.log("Status Persetujuan 1: ", this.state.statusPersetujuan1);
-    console.log("Status Persetujuan 1: ", this.state.statusPersetujuan1);
+    console.log("Status Persetujuan 2: ", this.state.statusPersetujuan2);
 
-    // Axios.post("http://localhost:3001/tambah_pesanan", {
-    //   nama_pemesanan: this.state.namaPemesan,
-    //   nama_kendaraan: this.state.namaKendaraan,
-    //   nama_penyetuju_1: this.state.namaPenyetuju1,
-    //   nama_penyetuju_2: this.state.namaPenyetuju2,
-    //   status_persetujuan_1: this.state.statusPersetujuan1,
-    //   status_persetujuan_2: this.state.statusPersetujuan2,
-    // });
+    Axios.post("http://localhost:3001/tambah_pesanan", {
+      nama_pemesan: this.state.namaPemesan,
+      nama_kendaraan: this.state.namaKendaraan,
+      nama_penyetuju_1: this.state.namaPenyetuju1,
+      nama_penyetuju_2: this.state.namaPenyetuju2,
+      status_persetujuan_1: this.state.statusPersetujuan1,
+      status_persetujuan_2: this.state.statusPersetujuan2,
+    });
+    this.validate();
   };
 
   render() {
@@ -55,7 +64,7 @@ export default class PemesananAdmin extends React.Component {
           <div className="container-fluid row mt-3">
             <div className="col-md-3"></div>
             <div className="col-md-6">
-              <div classNameName="form-group mt-3">
+              <div className="form-group mt-3">
                 <label>Nama Pemesan</label>
                 <div>
                   <select
@@ -71,7 +80,11 @@ export default class PemesananAdmin extends React.Component {
               <div className="form-group mt-3">
                 <label>Nama Kendaraan</label>
                 <div>
-                  <select>
+                  <select
+                    onChange={this.handleChange}
+                    value={this.state.namaKendaraan}
+                    name="namaKendaraan"
+                  >
                     <option value="1">Toyota Corolla</option>
                     <option value="2">Toyota Supra</option>
                   </select>
@@ -80,7 +93,11 @@ export default class PemesananAdmin extends React.Component {
               <div className="form-group mt-3">
                 <label>Nama Penyetuju 1</label>
                 <div>
-                  <select>
+                  <select
+                    onChange={this.handleChange}
+                    value={this.state.namaPenyetuju1}
+                    name="namaPenyetuju1"
+                  >
                     <option value="2">Penyetuju Satu</option>
                   </select>
                 </div>
@@ -88,15 +105,20 @@ export default class PemesananAdmin extends React.Component {
               <div className="form-group mt-3">
                 <label>Nama Penyetuju 2</label>
                 <div>
-                  <select>
+                  <select
+                    onChange={this.handleChange}
+                    value={this.state.namaPenyetuju2}
+                    name="namaPenyetuju2"
+                  >
                     <option value="3">Penyetuju Dua</option>
                     <option value="8">Penyetuju Dua B</option>
                   </select>
                 </div>
               </div>
               <button className="btn btn-primary mt-3">Submit</button>
+              <div>{this.state.infoSubmit}</div>
             </div>
-            <div className="col-md-3"></div>
+            <div className="col-md-3 mt-2"></div>
           </div>
         </form>
       </div>
